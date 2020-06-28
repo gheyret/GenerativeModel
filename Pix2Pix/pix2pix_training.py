dacd51787ebb5959be8ece39ccd9977ac287068c
Sho Watari <53390550+sho-watari@users.noreply.github.com>
2020-06-29 00:36:54 +0900

    Update pix2pix_training.py

--- Pix2Pix/pix2pix_training.py
+++ Pix2Pix/pix2pix_training.py
@@ -3,9 +3,9 @@ import cntk.io.transforms as xforms
 import cv2
 import numpy as np
 import os
+import pandas as pd
 
 from cntk.layers import BatchNormalization, Convolution2D, ConvolutionTranspose2D, Dropout
-from pandas import DataFrame
 
 img_channel = 3
 img_height = 256
@@ -201,7 +201,7 @@ if __name__ == "__main__":
     D_real.save("./pix2pix_discriminator.model")
     print("Saved model.")
 
-    df = DataFrame(logging)
+    df = pd.DataFrame(logging)
     df.to_csv("./pix2pix.csv", index=False)
     print("Saved logging.")
     
